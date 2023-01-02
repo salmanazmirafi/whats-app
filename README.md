9068397c27c01fd789813ea978973f247f3d41be
salmanazmirafi <116106498+salmanazmirafi@users.noreply.github.com>
2023-01-03 03:24:46 +0600

    r

--- README.md
+++
@@ -1,2 +0,0 @@
-# whats-app
- Technologies Used:  Frontend: React.js &amp; Material-UI Backend: Node.js &amp; Express.js Database: MongoDB Real Time Chatting: socket.io
---
+++ sharebackend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'covlrs6u',
+    dataset: 'production'
+  }
+})
