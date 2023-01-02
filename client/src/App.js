2253a1ced1f3634f5e9f037f7baa8f93208d310e
salmanazmirafi <116106498+salmanazmirafi@users.noreply.github.com>
2023-01-03 02:56:45 +0600

    ,

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import Messenger from "./components/Messenger";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { AccountProvide } from "./context/AccountProvide";
 
 function App() {
   const clientId =
     "993133641971-in8laen2m1ktr86lt5pkdo7s869ao85i.apps.googleusercontent.com";
   return (
     <GoogleOAuthProvider clientId={clientId}>
-      <Messenger />
+      <AccountProvide>
+        <Messenger />
+      </AccountProvide>
     </GoogleOAuthProvider>
   );
 }
---
+++ client/src/components/chat/ChatDialog.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ChatDialog() {
+  return <div></div>;
+}
+
+export default ChatDialog;
